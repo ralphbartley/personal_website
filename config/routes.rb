a86e16aac702dcd0f3498f2465b6d5996f09de10
ralphbartley <ralphgbartley@gmail.com>
2021-08-29 20:05:05 +0100

    added Projects and Blog views, displayed on Home index, tests for their presence

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'blog/index'
+  get 'projects/index'
   root 'home#index'
   get 'home/index'
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,14 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get home_index_url
     assert_response :success
   end
+
+  test "should link to projects" do
+    get root_url
+    assert_select "a", text: "Projects"
+  end
+
+  test "should link to blog" do
+    get root_url
+    assert_select "a", text: "Blog"
+  end
 end
