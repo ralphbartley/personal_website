adbd10c225afcebe1aa317c22d5318e734b778f8
= <ralphgbartley@gmail.com>
2021-09-22 16:59:25 +0100

    user email validation model and db

--- db/migrate/20210922155008_create_users.rb
+++ db/migrate/20210922155008_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.String :email
+      t.String :email, null: false
       t.String :password_digest
 
       t.timestamps
